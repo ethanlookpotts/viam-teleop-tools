da0067783bdfafb3575604834054b66e97435e44
zaporter-work <work-github@zackporter.com>
2024-10-28 13:43:41 -0400

    add globetrotter

--- module/main.go
+++ module/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"go.viam.com/rdk/components/movementsensor"
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 )
 
 var (
-	ModuleFamily    = resource.NewModelFamily("viz-team", "teleop")
-	EverythingModel = ModuleFamily.WithModel("everything")
+	ModuleFamily      = resource.NewModelFamily("viz-team", "teleop")
+	EverythingModel   = ModuleFamily.WithModel("everything")
+	GlobetrotterModel = ModuleFamily.WithModel("globetrotter")
 )
 
 func main() {
-	resource.RegisterComponent(sensor.API, EverythingModel, resource.Registration[resource.Resource, *EverythingConfig]{
+	resource.RegisterComponent(sensor.API, EverythingModel, resource.Registration[sensor.Sensor, *EverythingConfig]{
 		Constructor: newEverything,
 	})
 
-	module.ModularMain("everything", resource.APIModel{
-		API:   sensor.API,
-		Model: EverythingModel,
+	resource.RegisterComponent(movementsensor.API, GlobetrotterModel, resource.Registration[movementsensor.MovementSensor, *GlobetrotterConfig]{
+		Constructor: newGlobetrotter,
 	})
+
+	module.ModularMain(
+		"everything",
+		resource.APIModel{
+			API:   sensor.API,
+			Model: EverythingModel,
+		}, resource.APIModel{
+			API:   movementsensor.API,
+			Model: GlobetrotterModel,
+		})
 }
