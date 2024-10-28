6b4bed2d3e78167d579a818fa0611878a4f9320f
zaporter-work <work-github@zackporter.com>
2024-10-28 17:24:15 -0400

    fix run-with-auto-update

--- module/run-with-auto-update.sh
+++ module/run-with-auto-update.sh
@@ -8,7 +8,7 @@ if [[ "${TRACE-0}" == "1" ]]; then
 fi
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo 'Usage: ./run-with-auto-update.sh 
+    echo 'Usage: ./run-with-auto-update.sh
 
     Special script to get auto-updating behavior for local modules from pushes to main.
 
@@ -21,6 +21,5 @@ fi
 script_dir="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 pushd "$script_dir/.."
-git pull
-just build
-timeout 300 ./dist/module "$1"
+su "$MACH_USER" /bin/sh -c ". ~/.profile && git pull --ff && just build"
+timeout 300 ./dist/module "$@"
